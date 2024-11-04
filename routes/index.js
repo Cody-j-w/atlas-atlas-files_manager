0a2e483426c867f421adf885e4d55975983e2422
chdrchz <saavannaa.rose@gmail.com>
2024-11-04 15:09:32 -0600

    believe I finished up Authorization and X-token stuff

--- routes/index.js
+++ routes/index.js
@@ -10,8 +10,10 @@ router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
 
 router.get('/connect', AuthController.getConnect);
+router.get('/disconnect', AuthController.getDisconnect);
 
 router.post('/users', UsersController.postNew);
+router.get('/users/me', UsersController.getMe);
 
 router.post('/files', FilesController.postUpload);
 
