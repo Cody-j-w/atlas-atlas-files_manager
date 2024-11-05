a8522458e9bd4a76eea07dbef58aa8470e18ef74
Cody Walenciak <codywalenciak@gmail.com>
2024-11-05 10:39:30 -0600

    Add DB object construction for uploaded files

--- controllers/FilesController.js
+++ controllers/FilesController.js
@@ -1,3 +1,8 @@
+const { uuidV4 } = require('mongodb/lib/core/utils.js');
+const dbClient = require('../utils/db.js');
+const redisClient = require('../utils/redis.js');
+const fs = require('fs');
+
 let folderPath = '';
 if (!process.env.FOLDER_PATH || process.env.FOLDER_PATH === '') {
     folderPath = '/tmp/files_manager';
@@ -25,9 +30,32 @@ class FilesController {
         }
         let parentId = 0;
         parentId = req.body.parentId;
-        // if (parentId !== 0) {
-            
-        // }
+        if (parentId !== 0) {
+            const files = dbClient.db.collection('files');
+            const parent = await files.findOne({_id: parentId});
+            if (!parent) {
+                res.status(400).send('Parent not found');
+            } else if (parent.type !== 'folder') {
+                res.status(400).send('Parent is not a folder');
+            } else {
+                folderPath = parent.name;
+            }
+        }
+        if (type !== 'folder') {
+            const fileName = new uuidV4();
+
+        } else {
+            const newFile = {
+                userId: userId,
+                name: req.body.name,
+                type: req.body.type,
+                
+            }
+            fs.mkdir(req.body.name, (err) => {
+
+            })
+        }
+
     }
 }
 
