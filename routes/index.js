3da048527bb628b4b831ce1c4bbbf43691186335
Cody Walenciak <codywalenciak@gmail.com>
2024-11-07 14:55:09 -0600

    Add getFile route

--- routes/index.js
+++ routes/index.js
@@ -18,7 +18,7 @@ router.get('/users/me', UsersController.getMe);
 router.post('/files', FilesController.postUpload);
 router.get('/files', FilesController.getIndex);
 router.get('/files/:id', FilesController.getShow);
-router.get('/files/:id/publish', FilesController.putPublish);
-router.get('/files/:id/publish', FilesController.putUnpublish);
+
+router.get('/files/:id/data', FilesController.getFile);
 
 module.exports = router;
